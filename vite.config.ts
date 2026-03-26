030827f2a791afdd17465fa04cdc1cb42bbc5f66
zuker-joshua <90483754+zuker-joshua@users.noreply.github.com>
2026-03-26 17:34:23 -0400

    Configure Vite server with API proxy
    Added server configuration with proxy settings for API.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,30 @@
 import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      // Forward to fastAPI to connect to backend.
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+})
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
